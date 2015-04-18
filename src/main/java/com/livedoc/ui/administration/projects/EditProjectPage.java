a4be3ecd55b1ae44e2ed4a29f3247c5bc9c4a9c6
Alena <velikanova.alenka@gmail.com>
2015-04-18 19:50:06 +0400

    create project page

---
+++ src/main/java/com/livedoc/ui/administration/projects/EditProjectPage.java
@@ -0,0 +1,88 @@
+package com.livedoc.ui.administration.projects;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.validation.validator.StringValidator;
+
+import com.livedoc.bl.domain.entities.Project;
+import com.livedoc.bl.services.ProjectService;
+import com.livedoc.ui.components.Feedback;
+import com.livedoc.ui.pages.MasterPage;
+
+public class EditProjectPage extends MasterPage {
+
+	private static final long serialVersionUID = 6391339627405361984L;
+
+	@SpringBean
+	private ProjectService projectService;
+	
+	private Feedback feedbackPanel;
+	private Page pageToReturn;
+	private Form<Project> form;
+
+	public EditProjectPage(Page pageToReturn) {
+		super();
+		this.pageToReturn = pageToReturn;
+	}
+
+	@Override
+	public void onInitialize() {
+		super.onInitialize();
+		form = new Form<Project>("form", new Model<Project>(new Project()));
+		form.setOutputMarkupId(true);
+		add(form);
+
+		feedbackPanel = new Feedback("feedback");
+		feedbackPanel.setOutputMarkupId(true);
+		form.add(feedbackPanel);
+
+		TextField<String> projectNameField = new TextField<String>(
+				"project-name", new PropertyModel<String>(form.getModel(),
+						"name"));
+		projectNameField.add(StringValidator.maximumLength(32));
+		projectNameField.setRequired(true);
+
+		TextArea<String> projectDescriptionField = new TextArea<String>(
+				"project-description", new PropertyModel<String>(
+						form.getModel(), "description"));
+		projectDescriptionField.add(StringValidator.maximumLength(256));
+
+		AjaxButton saveButton = new AjaxButton("save-button", form) {
+
+			private static final long serialVersionUID = 131954482957027000L;
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				Project project = (Project) form.getModelObject();
+				projectService.saveProject(project);
+				setResponsePage(pageToReturn);
+			}
+
+			@Override
+			protected void onError(AjaxRequestTarget target, Form<?> form) {
+				target.add(feedbackPanel);
+			}
+		};
+
+		AjaxLink<Void> cancelButton = new AjaxLink<Void>("cancel-button") {
+
+			private static final long serialVersionUID = 3159133009240454583L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				//TODO confirmation dialog
+				setResponsePage(pageToReturn);
+			}
+		};
+		form.add(saveButton, cancelButton);
+		form.add(projectNameField, projectDescriptionField);
+	}
+}
--- src/main/java/com/livedoc/ui/administration/projects/ProjectsManagementPage.java
+++ src/main/java/com/livedoc/ui/administration/projects/ProjectsManagementPage.java
@@ -1,16 +1,38 @@
 package com.livedoc.ui.administration.projects;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+
+import com.livedoc.bl.domain.entities.Project;
 import com.livedoc.ui.administration.AdministrationPage;
+import com.livedoc.ui.common.components.table.Settings;
+import com.livedoc.ui.common.components.table.Table;
 
 public class ProjectsManagementPage extends AdministrationPage {
 
 	private static final long serialVersionUID = 6366226078694121949L;
 	
 	@Override
-	public void onInitialize()
-	{
+	public void onInitialize() {
 		super.onInitialize();
-		
+
+		AjaxLink<Void> createProjectLink = new AjaxLink<Void>("createProject") {
+			private static final long serialVersionUID = -6420561414459574501L;
+
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				EditProjectPage page = new EditProjectPage(ProjectsManagementPage.this);
+				setResponsePage(page);
+			}
+		};
+		add(createProjectLink);
+		Settings settings = new Settings();
+		settings.setRowCount(5);
+		settings.addItem("name", getString("table.projects.name"), 1);
+		settings.addItem("description", getString("table.projects.description"), 2);
+		Table<Project, String> table = new Table<Project, String>("projects-table",
+				settings, new ProjectsProvider());
+		add(table);
 	}
 
 }
