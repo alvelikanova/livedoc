643ba9401983592b52a2546b5774000cf50185a6
Alena <velikanova.alenka@gmail.com>
2015-05-13 00:31:47 +0400

    document view

--- src/main/java/com/livedoc/ui/documents/DocumentPanel.java
+++ src/main/java/com/livedoc/ui/documents/DocumentPanel.java
@@ -3,23 +3,38 @@ package com.livedoc.ui.documents;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.wicket.injection.Injector;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.model.ChainingModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.dom4j.Document;
 
 import com.livedoc.bl.domain.entities.DocumentData;
+import com.livedoc.bl.services.DocumentService;
+import com.livedoc.bl.services.DocumentTransformationsService;
+import com.livedoc.ui.common.components.MarkupProviderPanel;
 
 public class DocumentPanel extends GenericPanel<DocumentData> {
 
 	private static final long serialVersionUID = 6585895264284178455L;
 
+	// services
+	@SpringBean
+	private DocumentService documentService;
+	@SpringBean
+	private DocumentTransformationsService documentTransformationsService;
+
+	// components
 	private WebMarkupContainer documentContentContainer;
+	private MarkupProviderPanel markupPanel;
 
 	public DocumentPanel(String id, IModel<DocumentData> model) {
 		super(id, model);
+		Injector.get().inject(this);
 	}
 
 	@Override
@@ -43,6 +58,10 @@ public class DocumentPanel extends GenericPanel<DocumentData> {
 
 		documentContentContainer = new WebMarkupContainer("documentContent");
 		add(documentContentContainer);
+
+		markupPanel = new MarkupProviderPanel("markupPanel", new DocumentModel(
+				getModel()));
+		documentContentContainer.add(markupPanel);
 	}
 
 	class DateToStringModel extends ChainingModel<String> {
@@ -61,5 +80,28 @@ public class DocumentPanel extends GenericPanel<DocumentData> {
 			Date date = (Date) getChainedModel().getObject();
 			return dateFormat.format(date);
 		}
+	}
+
+	class DocumentModel extends ChainingModel<String> {
+
+		private static final long serialVersionUID = -6596111106515977616L;
+
+		public DocumentModel(IModel<DocumentData> modelObject) {
+			super(modelObject);
+		}
+
+		public String getObject() {
+			DocumentData docData = (DocumentData) this.getChainedModel()
+					.getObject();
+			if (docData != null) {
+				Document document = documentService
+						.buildDocument(getModelObject());
+				String html = documentTransformationsService
+						.transformXMLToString(document);
+				return html;
+			}
+			return null;
+		}
+
 	}
 }
